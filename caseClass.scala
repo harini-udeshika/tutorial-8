86651bd7fc8bfb4df19f087d27a0d35fcd489e64
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2022-09-19 23:02:27 +0530

    Add files via upload

---
+++ caseClass.scala
@@ -0,0 +1,40 @@
+import scala.math._
+object caseClass extends App {
+
+    case class Point(a:Double,b:Double) {
+      def x: Double = a;
+
+      def y: Double = b;
+
+      //+ Operator
+      def +(point: Point) = Point(this.x + point.x, this.y + point.y);
+
+      //move the point
+      def move(dx: Int, dy: Int) = Point(this.x + dx, this.y + dy);
+
+      //Distance between 2 given points
+      def distance(p1: Point): Double = {
+        val y2: Double = pow(p1.y - this.y, 2);
+        val x2: Double = pow(p1.x - this.x, 2);
+        return sqrt(y2 + x2);
+      }
+
+      //switch x and y
+      def invert() = Point(this.y, this.x);
+    }
+    val p1=Point(7,8);
+    val p2=Point(4,5);
+
+    //call +
+    println(p1+" + "+p2+"= "+ p1.+(p2));
+
+    //move the point by (1,1)
+    println(p1+" Move by (1,1) :"+ p1.move(1,1));
+
+    //distance between p1 and p2
+    println("Distance between "+p1+" and "+p2+" = " + p1.distance(p2));
+
+    //invert x and y
+    println("Invert of "+p1+" = "+ p1.invert());
+
+}
